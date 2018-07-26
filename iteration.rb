53c99ccc6f450580cf43314796a0d140564ee266
mairas45 <mairasackitey45@icloud.com>
2018-07-26 23:29:15 +0000

    automatically backed up by learn

--- iteration.rb
+++ iteration.rb
@@ -2,3 +2,7 @@ users = [ "maira", "nicoletta", "isabella", "rachana"]
 users.each do |name|
   puts "welcome #{name}"
 end
+
+users_age = {"rachana" => 30, "nicoletta" => 14, "isabella" => 15, "maira" => 14}
+
+puts "hello #{name} you are #{age} years old!"
